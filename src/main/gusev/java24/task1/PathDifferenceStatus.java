6fce3c5e422752fc4b2791b7f2ac921d49c1c5da
Danila <igusevdanila@gmail.com>
2024-01-13 17:41:45 +0400

    first task

---
+++ src/main/gusev/java24/task1/PathDifferenceStatus.java
@@ -0,0 +1,15 @@
+package gusev.java24.task1;
+
+public enum PathDifferenceStatus {
+    NotExists,
+    SameFile,
+    BiggerFile,
+    SmallerFile,
+    SameSizeFile,
+    SameDirectory,
+    SameAbsoluteNameDepth,
+    SamePrefix,
+    SameRoot,
+    Subpath,
+    ParentPath
+}
---
+++ src/test/gusev/java24/task1/PathFinderTest.java
@@ -0,0 +1,49 @@
+package gusev.java24.task1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathFinderTest {
+
+    @Test
+    void difference() throws IOException {
+        String p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\1.txt";
+        String p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\2.txt";
+        Path path1 = Path.of(p1);
+        Path path2 = Path.of(p2);
+    /*  String text = Files.readString(path1);
+        System.out.println(text);
+        text = Files.readString(path2);
+        System.out.println(text);*/
+        List<PathDifferenceStatus> answer = PathFinder.difference(path1, path2);
+        assertEquals(5, answer.size());
+        assertEquals(PathDifferenceStatus.SmallerFile, answer.get(0));
+        assertEquals(PathDifferenceStatus.SameDirectory, answer.get(1));
+        assertEquals(PathDifferenceStatus.SameAbsoluteNameDepth, answer.get(2));
+        assertEquals(PathDifferenceStatus.SamePrefix, answer.get(3));
+        assertEquals(PathDifferenceStatus.SameRoot, answer.get(4));
+        p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\3.txt";
+        p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\2.txt";
+        path1 = Path.of(p1);
+        path2 = Path.of(p2);
+        answer = PathFinder.difference(path1, path2);
+        assertEquals(1, answer.size());
+        assertEquals(PathDifferenceStatus.NotExists, answer.get(0));
+        p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\1.txt";
+        p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\nextLVL\\1.txt";
+        path1 = Path.of(p1);
+        path2 = Path.of(p2);
+        answer = PathFinder.difference(path1, path2);
+        assertEquals(4, answer.size());
+        assertEquals(PathDifferenceStatus.BiggerFile, answer.get(0));
+        assertEquals(PathDifferenceStatus.SamePrefix, answer.get(1));
+        assertEquals(PathDifferenceStatus.SameRoot, answer.get(2));
+        assertEquals(PathDifferenceStatus.Subpath, answer.get(3));
+    }
+}
