01620055fcf4c69a1ce1f7f90f5b9eca2d00a3e7
Danila <igusevdanila@gmail.com>
2024-01-17 15:24:38 +0400

    first task update

--- src/main/gusev/java24/task1/PathFinder.java
+++ src/main/gusev/java24/task1/PathFinder.java
@@ -1,5 +1,6 @@
 package gusev.java24.task1;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,96 +10,78 @@ import java.util.List;
 public class PathFinder {
     public static List<PathDifferenceStatus> difference(Path path1, Path path2) throws IOException {
         List<PathDifferenceStatus> answer = new LinkedList<>();
-        if (Files.notExists(path1) || Files.notExists(path2)) {
+        if (isNotExists(path1, path2)) {
             answer.add(PathDifferenceStatus.NotExists);
             return answer;
         }
-        if (Files.isSameFile(path1, path2))
+        File file1 = path1.toFile();
+        File file2 = path2.toFile();
+        if (file1.isFile() && file2.isFile() && isSameFile(path1, path2))
             answer.add(PathDifferenceStatus.SameFile);
-        if (Files.size(path1) > Files.size(path2))
-            answer.add(PathDifferenceStatus.BiggerFile);
-        else if (Files.size(path1) < Files.size(path2))
-            answer.add(PathDifferenceStatus.SmallerFile);
-        else
-            answer.add(PathDifferenceStatus.SameSizeFile);
-        Path path1Parent = path1.getParent();
-        Path path2Parent = path2.getParent();
-        if (path1Parent.equals(path2Parent))
+        if (file1.isFile() && file2.isFile())
+            answer.add(getSizeFile(path1, path2));
+        if (isSameDirectory(path1, path2))
             answer.add(PathDifferenceStatus.SameDirectory);
-        int p1 = path1.getNameCount();
-        int p2 = path2.getNameCount();
-        if (p1 == p2)
+        if (isSameAbsoluteNameDepth(path1, path2))
             answer.add(PathDifferenceStatus.SameAbsoluteNameDepth);
-        Path path1Prefix = path1.getName(0);
-        Path path2Prefix = path2.getName(0);
-        if (path1Prefix.equals(path2Prefix))
+        if (isSamePrefix(path1, path2))
             answer.add(PathDifferenceStatus.SamePrefix);
-        Path path1Root = path1.getRoot();
-        Path path2Root = path2.getRoot();
-        if (path1Root.equals(path2Root))
+        if (isSameRoot(path1, path2))
             answer.add(PathDifferenceStatus.SameRoot);
-        int min = Math.min(p1, p2) - 1;
-        int i = 0;
-        boolean isSame = true;
-        while (i < min && isSame) {
-            Path path1Name = path1.getName(i);
-            Path path2Name = path2.getName(i);
-            if (!path1Name.equals(path2Name))
-                isSame = false;
-            i++;
-        }
-        if (isSame && p1 > p2)
-            answer.add(PathDifferenceStatus.ParentPath);
-        else if (isSame && p2 > p1)
+        if (isSubpath(path1, path2))
             answer.add(PathDifferenceStatus.Subpath);
+        else if (isParentPath(path1, path2))
+            answer.add(PathDifferenceStatus.ParentPath);
         return answer;
     }
 
-    public boolean isNotExists(Path path1, Path path2) {
+    public static boolean isNotExists(Path path1, Path path2) {
         return (Files.notExists(path1) || Files.notExists(path2));
     }
 
-    public boolean isSameFile(Path path1, Path path2) throws IOException {
+    public static boolean isSameFile(Path path1, Path path2) throws IOException {
         return (Files.isSameFile(path1, path2));
     }
 
-    public boolean isBiggerFile(Path path1, Path path2) throws IOException {
-        return (Files.size(path1) > Files.size(path2));
+    public static PathDifferenceStatus getSizeFile(Path path1, Path path2) throws IOException {
+        if (Files.size(path1) > Files.size(path2))
+            return PathDifferenceStatus.BiggerFile;
+        if (Files.size(path1) < Files.size(path2))
+            return PathDifferenceStatus.SmallerFile;
+        return PathDifferenceStatus.SameSizeFile;
     }
 
-    public boolean isSmallerFile(Path path1, Path path2) throws IOException {
-        return (Files.size(path1) < Files.size(path2));
-    }
-
-    public boolean isSameSizeFile(Path path1, Path path2) throws IOException {
-        return (Files.size(path1) == Files.size(path2));
-    }
-
-    public boolean isSameDirectory(Path path1, Path path2) {
+    public static boolean isSameDirectory(Path path1, Path path2) {
         Path path1Parent = path1.getParent();
         Path path2Parent = path2.getParent();
         return (path1Parent.equals(path2Parent));
     }
 
-    public boolean isSameAbsoluteNameDepth(Path path1, Path path2) {
+    public static boolean isSameAbsoluteNameDepth(Path path1, Path path2) {
+        path1 = path1.toAbsolutePath();
+        path2 = path2.toAbsolutePath();
         int p1 = path1.getNameCount();
         int p2 = path2.getNameCount();
         return (p1 == p2);
     }
 
-    public boolean isSamePrefix(Path path1, Path path2) {
+    public static boolean isSamePrefix(Path path1, Path path2) {
+        path1 = path1.toAbsolutePath();
+        path2 = path2.toAbsolutePath();
         Path path1Prefix = path1.getName(0);
         Path path2Prefix = path2.getName(0);
         return (path1Prefix.equals(path2Prefix));
     }
 
-    public boolean isSameRoot(Path path1, Path path2) {
+    public static boolean isSameRoot(Path path1, Path path2) {
         Path path1Root = path1.getRoot();
         Path path2Root = path2.getRoot();
         return (path1Root.equals(path2Root));
     }
 
-    public boolean isSubpath(Path path1, Path path2) {
+    public static boolean isSubpath(Path path1, Path path2) {
+        path1 = path1.toAbsolutePath();
+        path2 = path2.toAbsolutePath();
         int p1 = path1.getNameCount();
         int p2 = path2.getNameCount();
         int min = Math.min(p1, p2) - 1;
@@ -114,7 +97,9 @@ public class PathFinder {
         return (isSame && p1 < p2);
     }
 
-    public boolean isParentPath(Path path1, Path path2) {
+    public static boolean isParentPath(Path path1, Path path2) {
+        path1 = path1.toAbsolutePath();
+        path2 = path2.toAbsolutePath();
         int p1 = path1.getNameCount();
         int p2 = path2.getNameCount();
         int min = Math.min(p1, p2) - 1;
--- src/test/gusev/java24/task1/PathFinderTest.java
+++ src/test/gusev/java24/task1/PathFinderTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PathFinderTest {
 
     @Test
-    void difference() throws IOException {
+    void differenceExists() throws IOException {
         String p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\1.txt";
         String p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\2.txt";
         Path path1 = Path.of(p1);
@@ -28,18 +28,24 @@ class PathFinderTest {
         assertEquals(PathDifferenceStatus.SameAbsoluteNameDepth, answer.get(2));
         assertEquals(PathDifferenceStatus.SamePrefix, answer.get(3));
         assertEquals(PathDifferenceStatus.SameRoot, answer.get(4));
-        p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\3.txt";
-        p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\2.txt";
-        path1 = Path.of(p1);
-        path2 = Path.of(p2);
-        answer = PathFinder.difference(path1, path2);
+    }
+    @Test
+    void differenceNotExists() throws IOException {
+        String p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\3.txt";
+        String p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\2.txt";
+        Path path1 = Path.of(p1);
+        Path path2 = Path.of(p2);
+        List<PathDifferenceStatus> answer = PathFinder.difference(path1, path2);
         assertEquals(1, answer.size());
         assertEquals(PathDifferenceStatus.NotExists, answer.get(0));
-        p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\1.txt";
-        p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\nextLVL\\1.txt";
-        path1 = Path.of(p1);
-        path2 = Path.of(p2);
-        answer = PathFinder.difference(path1, path2);
+    }
+    @Test
+    void differenceCatalog() throws IOException{
+        String p1 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\1.txt";
+        String p2 = "C:\\TeamsGroup\\TeamsGroupJ2_01\\test\\nextLVL\\1.txt";
+        Path path1 = Path.of(p1);
+        Path path2 = Path.of(p2);
+        List<PathDifferenceStatus> answer = PathFinder.difference(path1, path2);
         assertEquals(4, answer.size());
         assertEquals(PathDifferenceStatus.BiggerFile, answer.get(0));
         assertEquals(PathDifferenceStatus.SamePrefix, answer.get(1));
